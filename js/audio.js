0c7f5035d6baaeac543627db1b7dc4e87ebff554
Olga Bogdanova <TomieForever@gmail.com>
2021-10-20 22:41:29 +0300

    feat: audio

---
+++ js/audio.js
@@ -0,0 +1,96 @@
+export function initAudio() {
+    window.addEventListener('load', () => initPlayer());
+   }
+
+   function initPlayer(){
+       const audio = document.querySelector('.player');
+       const prev = document.querySelector('.play-prev')
+       const actionButton = document.getElementById("action");
+       const next = document.querySelector('.prev-next');
+    
+       const progressBar = document.getElementById("progress-bar");
+        const currTime = document.getElementById("curr-time");
+        const durationTime = document.getElementById("duration");
+        const muteButton = document.getElementById("mute");
+        const volumeScale = document.querySelector(".volume");
+
+
+       actionButton.addEventListener('click', playAudio);
+       audio.addEventListener('timeupdate', audioProgress);
+       progressBar.addEventListener('click', audioChangeTime);
+       muteButton.addEventListener('click', audioMute);
+        volumeScale.addEventListener('change', audioChangeVolume);
+
+       let isPlay=false;
+       ///const audioPlayer= new Audio();
+
+       function playAudio(){
+           audio.src=playList[0].src;
+           audio.currentTime=0; 
+
+           if(isPlay===false){
+             audio.play();
+             isPlay=true;
+             actionButton.classList.toggle("pause");
+           }
+           else {
+               audio.pause();
+               isPlay=false;
+               actionButton.classList.toggle("pause");
+           }
+           if (durationTime.innerHTML == "00:00") {
+            durationTime.innerHTML = audioTime(audio.duration);
+        }
+        }
+
+        function audioTime(time) {
+            time = Math.floor(time);
+            let minutes = Math.floor(time / 60);
+            let seconds = Math.floor(time - minutes * 60);
+            let minutesVal = minutes;
+            let secondsVal = seconds;
+            if (minutes < 10) {
+                minutesVal = '0' + minutes;
+            }
+            if (seconds < 10) {
+                secondsVal = '0' + seconds;
+            }
+            return minutesVal + ":" + secondsVal;
+        }
+    
+        function audioProgress() {
+            progress = audio.currentTime / audio.duration;
+            progressBar.value = progress * 100;
+            currTime.innerHTML = audioTime(audio.currentTime);
+        }
+
+        function audioChangeTime(e) {
+            let bounds = e.target.getBoundingClientRect();
+            let mouseX = e.clientX - bounds.left;
+    
+            let progress = mouseX / progressBar.offsetWidth;
+            audio.currentTime = audio.duration * progress;
+        }
+
+        function audioChangeVolume() {
+            audio.volume = volumeScale.value / 100;
+            if (audio.volume == 0) {
+                muteButton.classList.toggle('true');
+            } else {
+                muteButton.classList.toggle('true');
+            }
+        }
+    
+        function audioMute() {
+            if (audio.volume === 0) {
+                audio.volume = volumeScale.value / 100;
+                muteButton.classList.toggle('true');
+            }
+            else {
+                audio.volume = 0;
+                muteButton.classList.toggle('true');
+            }
+        }
+
+
+   }
--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@ import { initClockAndCalendar } from './clock.js';
 import { initSlider } from './slider.js';
 import { initWeather } from './weather.js';
 import { initQuote } from './quote.js';
+import { initAudio} from './audio.js';
+import playList from './playList.js';
 
 initClockAndCalendar();
 initSlider();
 initWeather();
 initQuote();
+initAudio();
+console.log(playList);
