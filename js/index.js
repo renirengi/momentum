aa0fecbca7630fb2a3434a3723331a4a20d5c502
Olga Bogdanova <TomieForever@gmail.com>
2021-10-23 23:39:40 +0300

    feat: todolist

--- js/index.js
+++ js/index.js
@@ -3,11 +3,14 @@ import { initSlider } from './slider.js';
 import { initWeather } from './weather.js';
 import { initQuote } from './quote.js';
 import { AudioPlayerComponent } from './audio-player.component.js';
+import { initToDoList } from './toDoList.js';
+
 
 initClockAndCalendar();
 initSlider();
 initWeather();
 initQuote();
+initToDoList();
 
 
 window.customElements.define('audio-player', AudioPlayerComponent);
